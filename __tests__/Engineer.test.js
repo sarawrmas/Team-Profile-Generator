aa8291ccd48f055955b619683067d72a50dc7048
Sara <sara.m.adamski@gmail.com>
2021-05-26 12:36:46 -0500

    passing test for all employee objects

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,40 +1,49 @@
 const Engineer = require('../lib/Engineer.js');
-jest.mock('../lib/Engineer.js');
+// jest.mock('../lib/Engineer.js');
 
-test('gets engineer name', () => {
-    const engineer = new Engineer('John');
+test('creates an engineer object', () => {
+    const engineer = new Engineer('John', 90, "John@gmail.com");
 
     expect(engineer.name).toBe('John');
-    expect(engineer.getName()).toHaveProperty('name');
-});
-
-test('gets engineer id', () => {
-    const engineer = new Engineer('John');
-    engineer.id = 50;
-
     expect(engineer.id).toEqual(expect.any(Number));
-    expect(engineer.getId()).toHaveProperty('id');
-});
-
-test('gets engineer email', () => {
-    const engineer = new Engineer('John');
-    engineer.email = "john@gmail.com"
-
     expect(engineer.email).toEqual(expect.any(String));
-    expect(engineer.getEmail()).toHaveProperty('email');
-});
+    expect(engineer.role).toBe('Engineer');
+})
 
-test('gets engineer role', () => {
-    const engineer = new Engineer('John');
+// test('gets engineer name', () => {
+//     const engineer = new Engineer('John');
 
-    expect(engineer.getRole()).toBe('Engineer');
-    expect(engineer.getRole()).toHaveProperty('role');
-});
+//     expect(engineer.name).toBe('John');
+//     expect(engineer.getName()).toHaveProperty('name');
+// });
 
-test('gets engineer github', () => {
-    const engineer = new Engineer('John');
-    engineer.github = "johnhub";
+// test('gets engineer id', () => {
+//     const engineer = new Engineer('John');
+//     engineer.id = 50;
 
-    expect(engineer.github).toEqual(expect.any(String));
-    expect(engineer.getGithub()).toHaveProperty('github');
-});
+//     expect(engineer.id).toEqual(expect.any(Number));
+//     expect(engineer.getId()).toHaveProperty('id');
+// });
+
+// test('gets engineer email', () => {
+//     const engineer = new Engineer('John');
+//     engineer.email = "john@gmail.com"
+
+//     expect(engineer.email).toEqual(expect.any(String));
+//     expect(engineer.getEmail()).toHaveProperty('email');
+// });
+
+// test('gets engineer role', () => {
+//     const engineer = new Engineer('John');
+
+//     expect(engineer.getRole()).toBe('Engineer');
+//     expect(engineer.getRole()).toHaveProperty('role');
+// });
+
+// test('gets engineer github', () => {
+//     const engineer = new Engineer('John');
+//     engineer.github = "johnhub";
+
+//     expect(engineer.github).toEqual(expect.any(String));
+//     expect(engineer.getGithub()).toHaveProperty('github');
+// });
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,39 +1,48 @@
 const Manager = require('../lib/Manager.js');
-jest.mock('../lib/Manager.js');
+// jest.mock('../lib/Manager.js');
 
-test('gets manager name', () => {
-    const manager = new Manager('John');
+test('creates an manager object', () => {
+    const manager = new Manager('John', 90, "John@gmail.com");
 
     expect(manager.name).toBe('John');
-    expect(manager.getName()).toHaveProperty('name');
-});
-
-test('gets manager id', () => {
-    const manager = new Manager('John');
-    manager.id = 50;
-
     expect(manager.id).toEqual(expect.any(Number));
-    expect(manager.getId()).toHaveProperty('id');
-});
-
-test('gets manager email', () => {
-    const manager = new Manager('John');
-    manager.email = "john@gmail.com"
-
     expect(manager.email).toEqual(expect.any(String));
-    expect(manager.getEmail()).toHaveProperty('email');
-});
+    expect(manager.role).toBe('Manager');
+})
 
-test('gets manager role', () => {
-    const manager = new Manager('John');
+// test('gets manager name', () => {
+//     const manager = new Manager('John');
 
-    expect(manager.getRole()).toBe('Manager');
-    expect(manager.getRole()).toHaveProperty('role');
-});
+//     expect(manager.name).toBe('John');
+//     expect(manager.getName()).toHaveProperty('name');
+// });
 
-test('gets manager github', () => {
-    const manager = new Manager('John');
-    manager.officeNumber = 89;
+// test('gets manager id', () => {
+//     const manager = new Manager('John');
+//     manager.id = 50;
 
-    expect(manager.officeNumber).toEqual(expect.any(Number));
-});
+//     expect(manager.id).toEqual(expect.any(Number));
+//     expect(manager.getId()).toHaveProperty('id');
+// });
+
+// test('gets manager email', () => {
+//     const manager = new Manager('John');
+//     manager.email = "john@gmail.com"
+
+//     expect(manager.email).toEqual(expect.any(String));
+//     expect(manager.getEmail()).toHaveProperty('email');
+// });
+
+// test('gets manager role', () => {
+//     const manager = new Manager('John');
+
+//     expect(manager.getRole()).toBe('Manager');
+//     expect(manager.getRole()).toHaveProperty('role');
+// });
+
+// test('gets manager github', () => {
+//     const manager = new Manager('John');
+//     manager.officeNumber = 89;
+
+//     expect(manager.officeNumber).toEqual(expect.any(Number));
+// });
